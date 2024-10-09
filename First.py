3b0e3b365e1c525bf8a04ac1e77967a03b724d30
Kazzzakmaks <kazakmaximsergeevich@gmail.com>
2024-10-09 16:23:44 +0000

    Přidán první program: sudy_nebo_lichy

--- First.py
+++ First.py
@@ -1,9 +1,8 @@
 def sudy_nebo_lichy(cislo):
     if cislo % 2 == 0:
-        print(f"Číslo {cislo} je sudé")
+        return "Číslo {} je sudé".format(cislo)
     else:
-        print(f"Číslo {cislo} je liché")
+        return "Číslo {} je liché".format(cislo)
 
-# Volání funkce
-sudy_nebo_lichy(5)
-sudy_nebo_lichy(1000000)
+print(sudy_nebo_lichy(5))
+print(sudy_nebo_lichy(1000000))
