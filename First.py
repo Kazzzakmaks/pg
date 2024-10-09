bd3e1d87899e3cf8be081447989eda39f4e55898
Kazzzakmaks <kazakmaximsergeevich@gmail.com>
2024-10-09 20:10:45 +0300

    First.py

--- First.py
+++ First.py
@@ -1,9 +1,8 @@
-def sudy_nebo_lichy(cislo):
-    if cislo % 2 == 0:
-        print(f"Číslo {cislo} je sudé")
-    else:
-        print(f"Číslo {cislo} je liché")
-
-# Volání funkce
-sudy_nebo_lichy(5)
-sudy_nebo_lichy(1000000)
+def sudy_nebo_lichy(cislo): 
+    if cislo % 2 == 0: 
+        print("Číslo", cislo, "je sudé") 
+    else: 
+        print("Číslo",cislo ," je liché") 
+ 
+cislo = int(input("Zadejte čislo:")) 
+sudy_nebo_lichy(cislo)
