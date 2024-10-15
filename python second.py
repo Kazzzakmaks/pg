f1e0a4ac7b460437e8920e667e8fda9768ccfdaa
Kazzzakmaks <kazakmaximsergeevich@gmail.com>
2024-10-15 20:41:10 +0300

    python second.py

--- python second.py
+++ python second.py
@@ -1,29 +1,28 @@
 def cislo_text(cislo):
-    # Slovníky pro převod čísel na text
-    jednotky = ["nula", "jedna", "dva", "tři", "čtyři", "pět", "šest", "sedm", "osm", "devět"]
-    desitky_do_20 = ["deset", "jedenáct", "dvanáct", "třináct", "čtrnáct", "patnáct", "šestnáct", "sedmnáct", "osmnáct", "devatenáct"]
-    desitky = ["", "", "dvacet", "třicet", "čtyřicet", "padesát", "šedesát", "sedmdesát", "osmdesát", "devadesát"]
-    sto = "sto"
-    
-    # Převod řetězce na celé číslo
+    # Převod vstupního čísla na celé číslo
     cislo = int(cislo)
     
-    # Textová reprezentace čísla
-    if cislo == 100:
-        return sto
-    elif cislo < 10:
+    jednotky = ["nula", "jedna", "dva", "tři", "čtyři", "pět", "šest", "sedm", "osm", "devět"]
+    desitky_do_20  = ["jedenáct", "dvanáct", "třináct", "čtrnáct", "patnáct", "šestnáct", "sedmnáct", "osmnáct", "devatenáct"]
+    desitky = ["", "deset", "dvacet", "třicet", "čtyřicet", "padesát", "šedesát", "sedmdesát", "osmdesát", "devadesát"]
+
+    if 0 <= cislo < 10:
         return jednotky[cislo]
-    elif 10 <= cislo < 20:
-        return desitky_do_20[cislo - 10]
+    elif 10 < cislo < 20:
+        return desitky_do_20 [cislo - 11]
+    elif cislo == 10:
+        return desitky[1]
+    elif 20 <= cislo < 100:
+        deset = desitky[cislo // 10]
+        jednotka = jednotky[cislo % 10]
+        return deset if jednotka == "nula" else f"{deset} {jednotka}"
+    elif cislo == 100:
+        return "sto"
     else:
-        desitkova_cast = desitky[cislo // 10]
-        jednotkova_cast = jednotky[cislo % 10]
-        if cislo % 10 == 0:
-            return desitkova_cast  # Např. 20, 30, 40
-        else:
-            return desitkova_cast + " " + jednotkova_cast  # Např. 25 -> "dvacet pět"
+        return "Číslo mimo rozsah"
 
 if __name__ == "__main__":
-    cislo = input("Zadej číslo od 0 do 100: ")
+    cislo = input("Zadej číslo: ")
     text = cislo_text(cislo)
     print(text)
+
